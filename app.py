24d4e53192998189e751364482b2012dd03b0851
Dmitry Tumanenko <tumanenko@seznam.cz>
2021-08-09 21:32:29 +0200

    working hello world

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request
 import logging
 
 logging.basicConfig(filename='flask-app.log', level=logging.DEBUG)
@@ -6,11 +6,11 @@ logging.basicConfig(filename='flask-app.log', level=logging.DEBUG)
 app = Flask(__name__)
 
 
-@app.route("/profile")
+@app.route("/")
 def profile():
-    logging.info("Request URL: " + request.url +
-                 "Request headers: " + request.headers +
-                 "Request body" + request.body)
+    # logging.info("Request URL: " + request.url +
+    #              "Request headers: " + request.headers +
+    #              "Request body" + request.body)
     if request.method == 'POST':
         return {
             "username": "Семён",
