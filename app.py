615aeaa43999c0a73f471796916caf0a7f553245
Dmitry Tumanenko <tumanenko@seznam.cz>
2021-08-09 22:38:47 +0200

    init v2

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, request
 import logging
 
-logging.basicConfig(filename='flask-app.log', level=logging.DEBUG)
+logging.basicConfig(filename='flask-app.log', level=logging.DEBUG,
+                    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s %(lineno)s %(args)s")
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def profile():
-    # logging.info("Request URL: " + request.url +
-    #              "Request headers: " + request.headers +
-    #              "Request body" + request.body)
+    try:
+        logging.info("Request URL: " + request.url +
+                     "Request headers: " + str(list(request.headers)) +
+                     "Request body" + str(list(request.body)))
+    except Exception as er:
+        logging.exception(str(er))
     if request.method == 'POST':
         return {
             "username": "Семён",
