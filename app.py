7e0b7fdd3e8a83a2f082147eb01c2dc959e5583c
Dmitry Tumanenko <tumanenko@seznam.cz>
2021-08-09 20:22:09 +0200

    testing

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
+import logging
+
+logging.basicConfig(filename='flask-app.log', level=logging.DEBUG)
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    return render_template("index.html")
+    logging.info("Request URL: " + request.url +
+                 "Request headers: " + request.headers +
+                 "Request body" + request.body)
+    if request.method == 'POST':
+        return render_template("index.html")
+    return "<p>Hello, World!</p>"
